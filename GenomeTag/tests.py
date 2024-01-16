50711048adf5a7516c485d4b098dc24df98d216a
Rosanne PHEBE <rosanne.phebe@universite-paris-saclay.fr>
2024-01-16 16:29:14 +0100

    updated tests

--- GenomeTag/tests.py
+++ GenomeTag/tests.py
@@ -10,11 +10,24 @@ class YourTestCase(TestCase):
         self.user_annotator = User.objects.create_user(username="marie", email="annotator@example.com", password="mariepass", role="annotator")
         self.user_reviewer = User.objects.create_user(username="alice", email="reviewer@example.com", password="alicepass", role="reviewer")
 
-def test_create_annotation(self):
+def test_create_annotation_annotator(self):
     self.client.login(username="marie", password="mariepass")
-
     # Make a request to the create_annotation view
     response = self.client.get('create/') 
-
     # Assert that the response status code is 200 (OK)
     self.assertEqual(response.status_code, 200)
+
+def test_create_annotation_reviewer(self):
+    self.client.login(username="alice", password="alicepass")
+    # Make a request to the create_annotation view
+    response = self.client.get('create/') 
+    # Assert that the response status code is 200 (OK)
+    self.assertEqual(response.status_code, 200)
+
+def test_create_annotation_viewer(self):
+    self.client.login(username="john", password="johnpass")
+    # Make a request to the create_annotation view
+    response = self.client.get('create/') 
+    # Assert that the response status code is 200 (OK)
+    self.assertEqual(response.status_code, 200) # change to error message or status code 
+
