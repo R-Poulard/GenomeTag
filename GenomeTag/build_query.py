244dfa4463e6d6d59430379284f82225e5ac4c3d
R-Poulard <remipoul@gmail.com>
2024-01-26 22:21:00 +0100

    fixed regex and cleaned up file

--- GenomeTag/build_query.py
+++ GenomeTag/build_query.py
@@ -1,10 +1,8 @@
-from GenomeTag.search_field import search_dic
 from django.db.models import Q, F
 from GenomeTag.models import Genome, Chromosome, Position, Annotation, Peptide
 from django.db.models import Count
-from django.db.models.functions import Length,Substr
+from django.db.models.functions import Length
 import re
-
 
 
 def check_query(form):
@@ -12,26 +10,22 @@ def check_query(form):
 
 
 def find_all_indices_regex(main_string, substring):
-            if substring[0]=='*':
-                substring=".{0,200}"+substring[1:]
-            pattern = re.compile(substring.replace("*","{0,200}"))
-            return [(match.start(),match.end()) for match in pattern.finditer(main_string)]
+    if substring[0] == '*':
+        substring = ".{0,200}"+substring[1:]
+    pattern = re.compile(substring.replace("*", "{0,200}"))
+    return [(match.start(), match.end()) for match in pattern.finditer(main_string)]
+
 
 def build_query(form):
-    form=dict(form)
-    print(form)
+    form = dict(form)
     res_list = []
     for i in range(len(form['text_field'])):
         dic = {"negation": int(form['negation'][i]), "condition": int(form['condition'][i]), "value": form["text_field"][i]}
-        print("dic inside loop", dic)
         res_list.append(single_query(form['result'][0], dic))
-    print(res_list)
     res = res_list[0]
-    print("debut",res)
     if 'connector' in form:
         for i in range(len(form['connector'])):
             if int(form['connector'][i]) == 1:
-                print("OR")
                 res = res.union(res_list[i+1])
             else:
                 res = res & res_list[i+1]
@@ -42,13 +36,9 @@ def build_query(form):
 def single_query(result, form):
     cls = Genome
     condition = Q()
-    print("---- debut single query---")
-    print(result)
     if result == "Genome":
-        print("ici")
         cls = Genome
         condition = ask_genome(form)
-        print(condition)
     elif result == "Chromosome":
         cls = Chromosome
         condition = ask_chromosome(form)
@@ -57,28 +47,23 @@ def single_query(result, form):
         condition = ask_peptide(form)
     elif result == "Annotation":
         cls = Annotation
-        condition = ask_annotation(form) & Q(status="u")
+        condition = ask_annotation(form) & Q(status="r")
     if form['negation'] == 1:
-        print("ici")
         return cls.objects.filter(~condition)
     else:
         return cls.objects.filter(condition)
 
 
 def ask_genome(form):
-    print("------- into ask genome")
-
     cond = form['condition']
-    #["Access Number","Number Chromosome", "Chromosome accession number", "in DOI", "in Text", "Species"
-    print(cond)
+    # ["Access Number","Number Chromosome", "Chromosome accession number", "in DOI", "in Text", "Species"
     if cond == 0:
-        print("la")
         return Q(id=form['value'])
     elif cond == 1:
         grouped_data = Chromosome.objects.values('genome').annotate(category_count=Count('genome'))
-        if form['value'][0]==">":
+        if form['value'][0] == ">":
             filtered_data = grouped_data.filter(category_count__gt=int(form['value'][1:]))
-        elif form['value'][0]=="<":
+        elif form['value'][0] == "<":
             filtered_data = grouped_data.filter(category_count__lt=int(form['value'][1:]))
         else:
             filtered_data = grouped_data.filter(category_count=int(form['value']))
@@ -98,37 +83,37 @@ def ask_genome(form):
 
 def ask_chromosome(form):
     # ["Access Number", "Start Position", "End Position", "Length", "Genome id", "Sequence", "Species"]
-
     cond = form['condition']
-    print(cond)
     if cond == 0:
-        print("la")
         return Q(accession_number=form['value'])
     elif cond == 1:
-        if form['value'][0]==">":
+        if form['value'][0] == ">":
             return Q(start__gt=int(form['value'][1:]))
-        elif form['value'][0]=="<":
+        elif form['value'][0] == "<":
             return Q(start__lt=int(form['value'][1:]))
         else:
             return Q(start=int(form['value']))
     elif cond == 2:
-        if form['value'][0]==">":
+        if form['value'][0] == ">":
             return Q(end__gt=int(form['value'][1:]))
-        elif form['value'][0]=="<":
+        elif form['value'][0] == "<":
             return Q(end__lt=int(form['value'][1:]))
         else:
             return Q(end=int(form['value']))
     elif cond == 3:
-        if form['value'][0]==">":
+        if form['value'][0] == ">":
             return Q(end__gt=F('start')+int(form['value'][1:]))
-        elif form['value'][0]=="<":
+        elif form['value'][0] == "<":
             return Q(end__lt=F('start')+int(form['value'][1:]))
         return Q(end=F('start')+int(form['value']))
     elif cond == 4:
         genome = Genome.objects.filter(id=form['value'])
         return Q(genome__in=genome)
     elif cond == 5:
-        return Q(sequence=form["value"])
+        substring = form['value']
+        if substring[0] == '*':
+            substring = ".{0,200}"+substring[1:]
+        return Q(sequence__regex=substring.replace('*', "{0,200}"))
     elif cond == 6:
         genome = Genome.objects.filter(species=form['value'])
         return Q(genome__in=genome)
@@ -136,19 +121,20 @@ def ask_chromosome(form):
 
 
 def ask_peptide(form):
-    #"Peptide": ["Access Number", "Proteique Sequence", "Size", "Tag id", "Annotation Acess Number","in Text"],
+    # "Peptide": ["Access Number", "Proteique Sequence", "Size", "Tag id", "Annotation Acess Number","in Text"],
     cond = form['condition']
-    print(cond)
     if cond == 0:
-        print("la")
         return Q(accesion=form['value'])
     elif cond == 1:
-        return Q(sequence=form['value'])
+        substring = form['value']
+        if substring[0] == '*':
+            substring = ".{0,200}"+substring[1:]
+        return Q(sequence__regex=substring.replace('*', '{0,200}'))
     elif cond == 2:
         grouped_data = Peptide.objects.annotate(length=Length('sequence'))
-        if form['value'][0]==">":
+        if form['value'][0] == ">":
             filtered_data = grouped_data.filter(length__gt=int(form['value'][1:]))
-        elif form['value'][0]=="<":
+        elif form['value'][0] == "<":
             filtered_data = grouped_data.filter(length__lt=int(form['value'][1:]))
         else:
             filtered_data = grouped_data.filter(length=int(form['value']))
@@ -164,57 +150,52 @@ def ask_peptide(form):
 
 
 def ask_annotation(form):
-    #"Annotation": ["Access Number", "Start Position","Start Relative Position", "End Position","End Relative Position","length","sequence", "strand", "Chromosome access number", "Genome access number", "Author id", "Tag id", "Element in text"]
+    # "Annotation": ["Access Number", "Start Position","Start Relative Position", "End Position","End Relative Position","length","sequence", "strand", "Chromosome access number", "Genome access number", "Author id", "Tag id", "Element in text"]
     cond = form['condition']
-    print(cond)
     if cond == 0:
-        print("la")
         return Q(accession=form['value'])
     elif cond == 1:
-        if form['value'][0]==">":
+        if form['value'][0] == ">":
             return Q(position__start__gt=int(form['value'][1:]))
-        elif form['value'][0]=="<":
+        elif form['value'][0] == "<":
             return Q(position__start__lt=int(form['value'][1:]))
         else:
             return Q(position__start=int(form['value']))
     elif cond == 2:
-        if form['value'][0]==">":
+        if form['value'][0] == ">":
             return Q(position__start_relative__gt=int(form['value'][1:]))
-        elif form['value'][0]=="<":
+        elif form['value'][0] == "<":
             return Q(position__start_relative__lt=int(form['value'][1:]))
         else:
             return Q(position__start_relative=int(form['value']))
     elif cond == 3:
-        if form['value'][0]==">":
+        if form['value'][0] == ">":
             return Q(position__end__gt=int(form['value'][1:]))
-        elif form['value'][0]=="<":
+        elif form['value'][0] == "<":
             return Q(position__end__lt=int(form['value'][1:]))
         else:
             return Q(position__end=int(form['value']))
     elif cond == 4:
-        if form['value'][0]==">":
+        if form['value'][0] == ">":
             return Q(position__end_relative__gt=int(form['value'][1:]))
-        elif form['value'][0]=="<":
+        elif form['value'][0] == "<":
             return Q(position__end_relative__lt=int(form['value'][1:]))
         else:
             return Q(position__end_relative=int(form['value']))
     elif cond == 5:
-        if form['value'][0]==">":
+        if form['value'][0] == ">":
             return Q(position__end__gt=F('position__start')+int(form['value'][1:]))
-        elif form['value'][0]=="<":
+        elif form['value'][0] == "<":
             return Q(position__end__lt=F('position__start')+int(form['value'][1:]))
         return Q(position__end=F('position__start')+int(form['value']))
     elif cond == 6:
-        chr_list= Chromosome.objects.all()
-        pos_list=[]
+        chr_list = Chromosome.objects.all()
+        pos_list = []
         for chr in chr_list:
-            print(chr.accession_number)
-            for pos in find_all_indices_regex(chr.sequence,form['value']):
-                print(pos)
-                start,end=pos
+            for pos in find_all_indices_regex(chr.sequence, form['value']):
+                start, end = pos
                 pos_list.extend(list(Position.objects.filter(
-                    chromosome=chr,start=start+1,end=end)))
-        #pos_list=[item.id for item in pos_list]
+                    chromosome=chr, start=start+1, end=end)))
         return Q(position__in=pos_list)
     elif cond == 7:
         return Q(position__strand=form['value'])
