713a20bac4d8f4c693e552297cce799eb6607baf
nathan.maligeay <nathan.maligeay@universite-paris-saclay.fr>
2024-02-05 18:29:14 +0100

    fuck this

--- GenomeTag/forms.py
+++ GenomeTag/forms.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-from .models import CustomUser, Annotation, Tag
+from .models import CustomUser, Genome, Annotation, Tag
 from django import forms
-
 
 class CustomUserCreationForm(UserCreationForm):
 
@@ -15,6 +14,30 @@ class CustomUserChangeForm(UserChangeForm):
         model = CustomUser
         fields = ("username", "email")
 
+        
+class SearchForm(forms.Form): 
+    result_type_choices = [
+        ('Genome', 'Genome'),
+        ('Chromosome', 'Chromosome'),
+        ('Peptide', 'Peptide'),
+        ('Annotation', 'Annotation'),
+    ]
+
+    result_type = forms.ChoiceField(choices=result_type_choices, label='')
+
+    class Media:
+        js = ('../projet_web/static/GenomeTag/search_form.js', )
+
+class ReviewForm(forms.Form):
+    Annotation = forms.CharField(widget=forms.HiddenInput(attrs={'readonly': 'readonly'}))
+    result_type_choices = [
+        ('refused', 'refused'),
+        ('validated', 'validated'),
+    ]
+    Status = forms.ChoiceField(choices=result_type_choices)
+    Author = forms.CharField(widget=forms.TextInput(attrs={'readonly': 'readonly'}))
+    Commentary = forms.CharField(widget=forms.Textarea())
+    
 class AnnotationForm(forms.ModelForm):
     class Meta:
         model = Annotation
@@ -30,4 +53,3 @@ class AnnotationForm(forms.ModelForm):
         self.fields['tags'].label = 'Tags'
         self.fields['tags'].required = False
         self.fields['tags'].widget.choices = [(tag.tag_id, tag.text) for tag in Tag.objects.all()]
-
